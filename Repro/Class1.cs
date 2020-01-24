bc5508304e1fdd5939fe4144d374fc48c8a1147a
Jamie Brynes <jamiebrynes@improbable.io>
2020-01-24 11:43:52 +0000

    repro

---
+++ Repro/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Repro
+{
+    public class ChildClass : BaseClass
+    {
+        public ChildClass() : base("", "") {}
+        
+        public ChildClass(string foo, string bar): base(foo ?? "", bar ?? "") {}
+    }
+
+    public class BaseClass
+    {
+        public BaseClass(string foo, string bar) {}
+    }
+}
